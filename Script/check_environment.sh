544ad73ebf364262e246ba63df7b72f5114023e6
badrish mishra <badrish.kumar.mishra@gmail.com>
2021-01-27 20:48:23 +0000

    Trim commit hash insterion in build file

--- Script/check_environment.sh
+++ Script/check_environment.sh
@@ -8,4 +8,8 @@ function version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$
 [ ${BREEZE_LSB_RELEASE} ]
 #[ ${BREEZE_KERNEL} ]
 [ ${BREEZE_PYTHON_VERSION} ]
+if [ "${BREEZE_LSB_ID}" != "RedHat" ] && [ "${BREEZE_LSB_ID}" != "CentOS" ] && [ "${BREEZE_LSB_ID}" != "Ubuntu" ]; then
+  echo "please use RHEL or CentOS or Ubuntu"
+  exit
+fi
 
