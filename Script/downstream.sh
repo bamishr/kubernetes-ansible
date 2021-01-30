8ef630de622e6fdd770bcf4590eaad6495b1ac89
badrish mishra <badrish.kumar.mishra@gmail.com>
2021-01-30 09:18:36 +0000

    Trim commit hash insterion in build file

--- Script/downstream.sh
+++ Script/downstream.sh
@@ -7,3 +7,21 @@ f_log_info()
     printf "%s:LOG:INFO: %s\n" "${0}" "${1}\n"
 }
 
+f_prep()
+{
+    f_log_info "${FUNCNAME[0]}"
+    # Array of excluded files from downstream build (relative path)
+    _file_exclude=(
+    )
+
+    # Files to copy downstream (relative repo root dir path)
+    _file_manifest=(
+        CHANGELOG.rst
+        galaxy.yml
+        LICENSE
+        README.md
+        Makefile
+        setup.cfg
+        .yamllint
+    )
+
